f4d9caf1c6bf0421f2425ed8aaf678422f5e6dd6
Josef Pospíšil <josef.pospisil@laststar.eu>
2017-04-07 14:28:29 +0200

    Initial commit

---
+++ test/cljs/laststar/app_test.cljs
@@ -0,0 +1,7 @@
+(ns laststar.app-test
+  (:require-macros [cljs.test :refer [deftest testing is]])
+  (:require [cljs.test :as t]
+            [laststar.app :as app]))
+
+(deftest test-arithmetic []
+  (is (= (+ 0.1 0.2) 0.3) "Something foul is a float."))
